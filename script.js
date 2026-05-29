e34ce39d082e6f0058532834aa04e6b9ef8c4c90
Satnam-Singh <sangherasoftware@gmail.com>
2026-05-29 14:07:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+window.addEventListener('scroll', () => {
+  const cards = document.querySelectorAll('.service-card');
+
+  cards.forEach(card => {
+    const top = card.getBoundingClientRect().top;
+
+    if(top < window.innerHeight - 100){
+      card.style.opacity = "1";
+      card.style.transform = "translateY(0)";
+    }
+  });
+});
